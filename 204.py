73648168201235b4fa416fe15c2e7f8ffd2f3ad5
ZhangHuaYan <648423370@qq.com>
2018-09-06 16:28:37 +0800

    test

--- 204.py
+++ 204.py
@@ -9,7 +9,7 @@
 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 
 '''
 
-
+#高效方法，核心：初始化一个全为false的列表，依次将2的倍数，3的倍数等置为true，然后返回true的个数
 import math
 def countPrimes(n):
     """
@@ -23,3 +23,34 @@ def countPrimes(n):
     for i in range(2, int(math.sqrt(n)) + 1):
         res[i * i:n:i] = [False] * len(res[i * i:n:i])
     return sum(res)
+
+
+print(countPrimes(4))
+
+
+class Solution:
+    #自制低效版
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return 0
+        sum = 0
+        for i in range(2, n):
+            if Solution.isPrime(i):
+                sum += 1
+        return sum
+
+    @staticmethod
+    def isPrime(n):
+        # 判断一个数是否为素数
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+
+
+
